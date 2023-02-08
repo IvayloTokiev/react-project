a09a533292351efb41100232c7e0ac1417b44e1a
Ivaylo Tokiev <ivaylo.tokiev@outdoorsy.co>
2023-02-08 17:02:31 +0200

    Adding footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./FooterStyles.css";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaSearchLocation,
+  FaTwitter,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaSearchLocation
+              size={20}
+              style={{ color: "#ffffff", marginRight: "2rem" }}
+            />
+            <div>
+              <p>123 Test St.</p>
+              <h4>Huston, Tx</h4>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />
+              1-888-123-1234
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />
+              plannet@mercury.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            elit mauris, vulputate vitae auctor ut, ornare at neque. Curabitur
+            vel enim quis ante ullamcorper porta.
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
